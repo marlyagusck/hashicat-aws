ff65e1be533274c4404360a40f4209f488e562a4
marlyagusck <marli.agustianto@cloud-klinetics.com>
2023-09-07 06:54:11 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3-bucket" {
   source  = "app.terraform.io/jokian/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "joki"
+  bucket_prefix = "joki"
   acl    = "private"
 
   versioning = {
@@ -10,4 +10,16 @@ module "s3-bucket" {
 
 }
 
+
+module "s3_bucket_for_logs" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.8.0"
+  bucket_prefix = "joki"
+  acl    = "log-delivery-write"
+
+  # Allow deletion of non-empty bucket
+  force_destroy = true
+
+  attach_elb_log_delivery_policy = true
+}
 
